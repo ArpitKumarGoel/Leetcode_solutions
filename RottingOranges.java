aac858e3148e1679868f6a31f547ad02d950e131
Arpit Goel <goelarpit286@gmail.com>
2026-06-20 15:18:20 +0530

    Implement solution for rotting oranges problem

---
+++ RottingOranges.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int[][] time = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            Arrays.fill(time[i], Integer.MAX_VALUE);
+        }
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(grid[i][j] == 2){
+                    dfs(grid, time, i, j, 0); 
+                } 
+            } 
+        } 
+        
+        int timeRequired = 0; 
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(grid[i][j] == 1){
+                    if(time[i][j] == Integer.MAX_VALUE){
+                        return -1;
+                    } 
+                    timeRequired = Math.max(timeRequired, time[i][j]); 
+                } 
+            } 
+        } 
+        return timeRequired; 
+    } 
+
+    private void dfs(int grid[][], int[][] time, int i, int j, int currentTime){
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0){
+            return; 
+        } 
+        if (grid[i][j] == 2 && currentTime > 0) {
+            return;
+        }
+        if(currentTime >= time[i][j]){
+            return;
+        }
+        
+        time[i][j] = currentTime; 
+        dfs(grid, time, i - 1, j, currentTime + 1); 
+        dfs(grid, time, i + 1, j, currentTime + 1); 
+        dfs(grid, time, i, j - 1, currentTime + 1);
+        dfs(grid, time, i, j + 1, currentTime + 1);
+    }
+}
